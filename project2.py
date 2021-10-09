cb3f61d160fc2d23b9cd847f61e2a8f9c950afe3
Connor <connor.neff77@gmail.com>
2021-10-09 14:57:25 -0500

    added main and node classes

--- project2.py
+++ project2.py
@@ -5,3 +5,43 @@ Created on Sat Oct  9 14:32:54 2021
 @author: conno
 """
 
+class node:
+    
+    def __init__(self):
+        self.name = ""
+        self.parentnode = ""
+        self.childnodes = []
+        self.adjacentnodes = []
+        
+    def setName(self,n):
+        self.name = n
+        
+    def getName(self):
+        return self.name
+        
+    def setParentNode(self,parent):
+        self.parentnode = parent
+        
+    def getParentNode(self):
+        return self.parentnode
+    
+    def setChildNodes(self,children):
+        self.childnodes = children
+    
+    def getChildNodes(self):
+        return self.childnodes
+    
+    def setAdjacentNodes(self,adjNodes):
+        self.adjacentnodes = adjNodes
+        
+    def getAdjacentNodes(self):
+        return self.adjacentnodes
+    
+def main():
+    a = node()
+    
+    
+if __name__ == "__main__":
+    main()
+    
+    
